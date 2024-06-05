1eb396525cbd91b941925364e5520ff91e63d8f7
eJafari <ebrahimaria@gmail.com>
2024-06-05 10:52:16 +0330

    Create per.php

---
+++ per.php
@@ -0,0 +1,11 @@
+<?php
+$translations = array(
+    'Change language' => ' تغییر زبان',
+    'per' => ' پارسی',
+    'Dashboard' => 'پیشخوان',
+    'Settings' => 'تنظیمات',
+    'Logout' => 'خروج',
+    'Comment' => 'توضیحات',
+    'Date' => 'تاریخ',
+);
+?>
